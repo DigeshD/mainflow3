4a297cdefb1e2378029fca3d9a4965d04f928d82
DigeshD <digeshdansana@gmail.com>
2024-07-26 00:28:16 +0530

    first

---
+++ includes/dbHandler.inc.php
@@ -0,0 +1,21 @@
+<?php
+
+// Define the Data Source Name (DSN) for the MySQL database connection
+$dsn = "mysql:host=localhost;dbname=my_project_db";
+
+// Set the database username and password
+$dbusername = "root";
+$dbpassword = "";
+
+// Attempt to establish a connection to the database
+try {
+    $pdo = new PDO($dsn, $dbusername, $dbpassword);
+
+    // Set the error mode to exception, which will throw an exception if an error occurs
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+// If a PDO exception is thrown, catch it and display an error message
+} catch (PDOException $err) {
+    echo "Connection Failed: ". $err->getMessage();
+    exit();
+}
